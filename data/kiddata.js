9352981f649321c7ce6d9aeaf5550969fd9e5721
diksha pathak <dikshapathak1420@gmail.com>
2023-09-17 22:04:20 +0530

    Worked on Creating Data for Website

---
+++ data/kiddata.js
@@ -0,0 +1,87 @@
+
+
+const kidsshoeProducts = [
+    {
+      id: 1 ,
+      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/32038e25-d625-46e0-9c07-38f50c6dabed/cortez-basic-shoes-cLQNtQ.png" ],
+      product_name: 'Nike Cortez Basic',
+      product_details: ' Baby/Toddler Shoes',
+      price: '₹ 2 995.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 2 ,
+      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c2de5c5b-f279-4bae-a541-75792a0a7dab/dynamo-go-younger-easy-on-off-shoes-dCf2Zv.png"],
+      product_name: 'Nike Dynamo Go',
+      product_details: 'Younger Kid\'s Easy On/Off Shoes',
+      price: '₹ 4 995.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 3 ,
+      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/241edead-9131-4552-9d55-9b1e6762e759/jr-phantom-gx-academy-younger-older-football-shoes-NDHwxc.png"   ],
+      product_name: 'Nike Jr. Phantom GX Academy',
+      product_details: 'Older Kid\'s Shoes',
+      price: '₹ 6 495.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 4 ,
+      image: [src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/889ef13a-6c68-406c-badd-713ccddc74f7/air-jordan-xxxviii-older-shoes-69P5Hc.png"  ],
+      product_name: 'Air Jordan XXXVIII',
+      product_details: 'Older Kid\'s Shoes',
+      price: '₹ 11 195.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 5 ,
+      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/706cd7f8-d5f7-4db3-a5f7-3577876a0afc/air-jordan-xxxviii-fiba-older-shoes-jsK87k.png"  ],
+      product_name: 'Air Jordan XXXVIII \'FIBA\' ',
+      product_details: 'Older Kid\'s Shoes',
+      price: '₹ 11 195.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 6 ,
+      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/8f27e765-7201-4cc8-b570-c76c301df01e/sky-jordan-1-and-shoe-td3n17.png"  ],
+      product_name: 'Sky Jordan 1',
+      product_details: ' Kid\'s Shoes',
+      price: '₹ 3 495.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 7 ,
+      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/eb5a3e14-23a6-44a2-831c-2a73729026df/air-jordan-1-low-se-older-shoes-xHjmvK.png"],
+      product_name: 'Air Jordan 1 Low SE',
+      product_details: 'Older Kid\'s Shoes',
+      price: '₹  7 877.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 8 ,
+      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/49b1eaf8-2115-423e-8856-a2907aefb611/jordan-1-low-alt-se-shoes-sSxw84.png" ],
+      product_name: 'Jordan 1 Low Alt SE',
+      product_details: 'Baby/Toddler Shoes',
+      price: '₹ 4 267.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 9 ,
+      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/0c972a21-c47a-40ae-937c-880f2b7b4a99/air-jordan-6-retro-low-older-shoes-znJNLw.png" ],
+      product_name: 'Air Jordan 6 Retro SE',
+      product_details: 'Older Kid\'s Shoes',
+      price: '₹ 8 295.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    },
+      {
+      id: 10 ,
+      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/4c2b7d5e-4c6b-4f94-9a4c-d83dda629c0a/air-jordan-5-retro-se-older-shoes-bQKcl5.png" ],
+      product_name: 'Air Jordan 5 Retro SE',
+      product_details: 'Older Kid\'s Shoes',
+      price: '₹ 11 295.00',
+      sizes: ['US 4', 'US 5', 'US 6', 'US 7', 'US 8'],
+    }
+    
+  ];
+  
+  
