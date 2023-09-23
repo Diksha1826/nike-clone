c7a143cd1611d7ae8d4441b0098fede2dbaba700
diksha pathak <dikshapathak1420@gmail.com>
2023-09-23 18:38:08 +0530

    worked on cart and new inner page of products

--- data/mensdata.js
+++ data/mensdata.js
@@ -1,91 +1,115 @@
 const mensshoeProducts = [
-    {
-      id: 1 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/bfc7efea-4b02-48f8-a00a-0318baf12316/sabrina-1-ionic-ep-basketball-shoes-ZK1Xwv.png" , "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/17ed15c1-353f-4051-bed6-69d350499a78/sabrina-1-spark-ep-basketball-shoes-ZK1Xwv.png"],
-      product_name: ' Sabrina 1 \'Ionic\' EP ',
-      product_details: 'Basketball Shoes',
-      price: 10795.00,
-      sizes: ['US 7', 'US 8', 'US 9', 'US 10', 'US 11'],
-    },
-    { 
-      id: 2 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/de63e1ff-c1ab-40e9-aaaf-1ccd5d0cac07/air-jordan-1-low-se-craft-shoes-q1ktlR.png"   ],
-      product_name: 'Air Jordan 1 Low SE Craft',
-      product_details: 'Men\'s Shoes',
-      price:  9777.00,
-      sizes: ['US 7', 'US 8', 'US 9', 'US 10', 'US 11'],
-    },
-   
-    {
-      id: 3 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/9782d869-ecb8-4be8-9f7c-4cae2a56769a/air-max-ap-shoes-3Rdq04.png" , "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d2b92a59-447d-49ed-b2a9-2562ebdb9ed5/air-max-ap-shoes-3Rdq04.png"] ,
-      product_name: 'Nike Air Max AP',
-      product_details: 'Men\'s Shoes',
-      price:  8295.00,
-      sizes: ['US 11', 'US 12', 'US 13', 'US 1', 'US 2'],
-    },
-    
-    {
-      id: 4 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ed45ceac-1a49-450c-a71d-3ffedbb376d0/infinityrn-4-road-running-shoes-mLRjcz.png" , "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fb5ad585-ec9c-4ab6-ab6f-b0b6b50235a4/infinityrn-4-road-running-shoes-mLRjcz.png" , "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/71a3d324-05c0-4b8d-9d78-0bd937520798/infinityrn-4-road-running-shoes-mLRjcz.png" ] ,
-      product_name: 'Nike InfinityRN 4',
-      product_details: 'Men\'s Road Running Shoes',
-      price: 14995.00,
-      sizes: ['US 7', 'US 8', 'US 9', 'US 10', 'US 11'],
-    },
-    {
-      id: 5 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f9da8586-6b8b-48c6-b820-cfc5e010474a/jordan-max-aura-4-shoes-cKMcXJ.png" , "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f9da8586-6b8b-48c6-b820-cfc5e010474a/jordan-max-aura-4-shoes-cKMcXJ.png" ],
-      product_name: 'Jordan Max Aura 4',
-      product_details: 'Men\'s Shoes',
-      price: 10727.00,
-      sizes: ['US 11', 'US 12', 'US 13', 'US 1', 'US 2'],
-    },
-    {
-      id: 6 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fad35cf8-33b9-48d7-9fa8-e801d742daf6/air-max-systm-shoes-hLmQ85.png" , "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/51fd150a-29bc-4385-b0b3-dd29f6487d60/air-max-systm-shoes-hLmQ85.png"],
-      product_name: 'Nike Air Max SYSTM',
-      product_details: 'Men\'s Shoes',
-      price:  8595.00,
-      sizes: ['US 6', 'US 7', 'US 8', 'US 9', 'US 10'],
-    },
-  
-    {
-      id: 7 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/60e98509-bbe4-4edf-a1cc-77567773fcef/dunk-low-retro-shoes-LX3n9P.png"  ] ,
-      product_name: 'Nike Dunk Low Retro Premium',
-      product_details: 'Men\'s Shoes',
-      price: 9207.00,
-      sizes: ['US 6', 'US 7', 'US 8', 'US 9', 'US 10'],
-    },
+  {
+    id: 1,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/bfc7efea-4b02-48f8-a00a-0318baf12316/sabrina-1-ionic-ep-basketball-shoes-ZK1Xwv.png",
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/17ed15c1-353f-4051-bed6-69d350499a78/sabrina-1-spark-ep-basketball-shoes-ZK1Xwv.png",
+    ],
+    product_name: " Sabrina 1 'Ionic' EP ",
+    product_details: "Basketball Shoes",
+    price: 10795.0,
+    sizes: ["US 7", "US 8", "US 9", "US 10", "US 11"],
+  },
+  {
+    id: 2,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/de63e1ff-c1ab-40e9-aaaf-1ccd5d0cac07/air-jordan-1-low-se-craft-shoes-q1ktlR.png",
+    ],
+    product_name: "Air Jordan 1 Low SE Craft",
+    product_details: "Men's Shoes",
+    price: 9777.0,
+    sizes: ["US 7", "US 8", "US 9", "US 10", "US 11"],
+  },
 
-    {
-      id: 8 ,
-      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/b1d3cf2e-0dca-4f6d-97ff-21006c346e8e/air-jordan-1-retro-high-og-shoes-Pz6fZ9.png" , "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-shoes-Pz6fZ9.png"],
-      product_name: 'Air Jordan 1 Retro High OG',
-      product_details: 'Men\'s Shoes',
-      price: 16147.00,
-      sizes: ['US 5', 'US 6', 'US 7', 'US 8', 'US 9'],
-    },
-    {
-      id: 9 ,
-      image: [ "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3727d0f9-a6d1-4ee9-993c-9aac2fd0b65b/air-force-1-high-07-lv8-shoes-RTQ6x1.png"] ,
-      product_name: 'Nike Air Force 1 High \'07 LV8',
-      product_details: 'Men\'s Shoes',
-      price: 7377.00,
-      sizes: ['US 5', 'US 6', 'US 7', 'US 8', 'US 9'],
-    },
-    {
-      id: 10 ,
-      image: ["https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/133d8e36-a9d2-4171-8fd3-6eabf13c483b/infinityrn-4-se-road-running-shoes-STv3Zz.png" ],
-      product_name: 'Nike InfinityRN 4 SE',
-      product_details: 'Men\'s Road Running Shoes ',
-      price: 14995.00,
-      sizes: ['US 7', 'US 8', 'US 9', 'US 10', 'US 11'],
-    }
-  
-    
-  ];
-  
-  localStorage.setItem("nikemensdata" , JSON.stringify(mensshoeProducts) );
-  
+  {
+    id: 3,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/9782d869-ecb8-4be8-9f7c-4cae2a56769a/air-max-ap-shoes-3Rdq04.png",
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/d2b92a59-447d-49ed-b2a9-2562ebdb9ed5/air-max-ap-shoes-3Rdq04.png",
+    ],
+    product_name: "Nike Air Max AP",
+    product_details: "Men's Shoes",
+    price: 8295.0,
+    sizes: ["US 11", "US 12", "US 13", "US 1", "US 2"],
+  },
+
+  {
+    id: 4,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ed45ceac-1a49-450c-a71d-3ffedbb376d0/infinityrn-4-road-running-shoes-mLRjcz.png",
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fb5ad585-ec9c-4ab6-ab6f-b0b6b50235a4/infinityrn-4-road-running-shoes-mLRjcz.png",
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/71a3d324-05c0-4b8d-9d78-0bd937520798/infinityrn-4-road-running-shoes-mLRjcz.png",
+    ],
+    product_name: "Nike InfinityRN 4",
+    product_details: "Men's Road Running Shoes",
+    price: 14995.0,
+    sizes: ["US 7", "US 8", "US 9", "US 10", "US 11"],
+  },
+  {
+    id: 5,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f9da8586-6b8b-48c6-b820-cfc5e010474a/jordan-max-aura-4-shoes-cKMcXJ.png",
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/f9da8586-6b8b-48c6-b820-cfc5e010474a/jordan-max-aura-4-shoes-cKMcXJ.png",
+    ],
+    product_name: "Jordan Max Aura 4",
+    product_details: "Men's Shoes",
+    price: 10727.0,
+    sizes: ["US 11", "US 12", "US 13", "US 1", "US 2"],
+  },
+  {
+    id: 6,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fad35cf8-33b9-48d7-9fa8-e801d742daf6/air-max-systm-shoes-hLmQ85.png",
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/51fd150a-29bc-4385-b0b3-dd29f6487d60/air-max-systm-shoes-hLmQ85.png",
+    ],
+    product_name: "Nike Air Max SYSTM",
+    product_details: "Men's Shoes",
+    price: 8595.0,
+    sizes: ["US 6", "US 7", "US 8", "US 9", "US 10"],
+  },
+
+  {
+    id: 7,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/60e98509-bbe4-4edf-a1cc-77567773fcef/dunk-low-retro-shoes-LX3n9P.png",
+    ],
+    product_name: "Nike Dunk Low Retro Premium",
+    product_details: "Men's Shoes",
+    price: 9207.0,
+    sizes: ["US 6", "US 7", "US 8", "US 9", "US 10"],
+  },
+
+  {
+    id: 8,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/b1d3cf2e-0dca-4f6d-97ff-21006c346e8e/air-jordan-1-retro-high-og-shoes-Pz6fZ9.png",
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/5927b4c8-6f28-4334-a174-20edb76c3c29/air-jordan-1-retro-high-og-shoes-Pz6fZ9.png",
+    ],
+    product_name: "Air Jordan 1 Retro High OG",
+    product_details: "Men's Shoes",
+    price: 16147.0,
+    sizes: ["US 5", "US 6", "US 7", "US 8", "US 9"],
+  },
+  {
+    id: 9,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/3727d0f9-a6d1-4ee9-993c-9aac2fd0b65b/air-force-1-high-07-lv8-shoes-RTQ6x1.png",
+    ],
+    product_name: "Nike Air Force 1 High '07 LV8",
+    product_details: "Men's Shoes",
+    price: 7377.0,
+    sizes: ["US 5", "US 6", "US 7", "US 8", "US 9"],
+  },
+  {
+    id: 10,
+    image: [
+      "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/133d8e36-a9d2-4171-8fd3-6eabf13c483b/infinityrn-4-se-road-running-shoes-STv3Zz.png",
+    ],
+    product_name: "Nike InfinityRN 4 SE",
+    product_details: "Men's Road Running Shoes ",
+    price: 14995.0,
+    sizes: ["US 7", "US 8", "US 9", "US 10", "US 11"],
+  },
+];
+
+localStorage.setItem("nikemensdata", JSON.stringify(mensshoeProducts));
--- script/cart.js
+++ script/cart.js
@@ -1,14 +1,22 @@
 var data = JSON.parse(localStorage.getItem("mencartarr"));
 
+result(data)
+totalcal();
+
 function result(data){
     document.querySelector("#cartcontainer").innerHTML = ""
 data.map(function(elem , index , array){
     var div = document.createElement("div");
-    div.setAttribute("class" , "inndiv");
+    div.setAttribute("class" , "cartinndiv");
+
+    var cartshoedetails = document.createElement("div");
+    cartshoedetails.setAttribute("class" , "cartshoedetail");
+
 
     var image = document.createElement("img");
     image.setAttribute("src" , elem.image[0]);
     image.setAttribute("class" , "nikeimg");
+
 
     var name = document.createElement("p");
     name.innerText = elem.product_name ;
@@ -16,53 +24,95 @@ data.map(function(elem , index , array){
     var description = document.createElement("p");
     description.innerText = elem.product_details ;
 
-    var quantity = document.createElement("p");
-    quantity.innerText = "qty-" + elem.qty;
+    // var quantity = document.createElement("p");
+    // quantity.innerText = "qty-" + elem.qty;
 
-    var incqty = document.createElement("button");
-    incqty.innerText = "Qty++";
-    incqty.addEventListener("click" , function(){
-        increaseqty(elem);
+    var quantity = document.createElement("select");
+    var option1 = document.createElement("option");
+    option1.innerText =    elem.qty;
+    var option2 = document.createElement("option");
+    option2.innerText = 2;
+    var option3 = document.createElement("option");
+    option3.innerText = 3;
+    var option4 = document.createElement("option");
+    option4.innerText = 4;
+    var option5 = document.createElement("option");
+    option5.innerText = 5;
+    var option6 = document.createElement("option");
+    option6.innerText = 6;
+    var option7 = document.createElement("option");
+    option7.innerText = 7;
+    var option8 = document.createElement("option");
+    option8.innerText = 8;
+    var option9 = document.createElement("option");
+    option9.innerText = 9;
+    var option10 = document.createElement("option");
+    option10.innerText = 10;
 
+    quantity.addEventListener("change" , function(){
+        quantitychange(quantity , elem)
     })
 
-    var decqty = document.createElement("button");
-    decqty.innerText = "Qty--";
-    decqty.addEventListener("click" , function(){
-        decreaseqty(elem);
+    quantity.append(option1 , option2 , option3 , option4 , option5 , option6 , option7 , option8 , option9 , option10)
 
-    })
+
+
+    // var incqty = document.createElement("button");
+    // incqty.innerText = "Qty++";
+    // incqty.addEventListener("click" , function(){
+    //     increaseqty(elem);
+
+    // })
+
+    // var decqty = document.createElement("button");
+    // decqty.innerText = "Qty--";
+    // decqty.addEventListener("click" , function(){
+    //     decreaseqty(elem);
+
+    // })
 
     var price = document.createElement("p");
-    price.innerText = "₹" + elem.price ;
+    price.setAttribute("class" , "cartshoeprice")
+    price.innerText = "MRP: ₹ " + elem.price ;
 
-    div.append(image , name , description , quantity , incqty, decqty , price);
+    var pricetopdiv = document.createElement("div");
+    pricetopdiv.setAttribute("class" , "pricetopdivcart");
 
 
-    document.querySelector("#cartcontainer").append(div );
+    pricetopdiv.append( name , price);
 
 
-})
-}
-result(data)
-totalcal();
+    cartshoedetails.append(pricetopdiv ,description, quantity );
 
-function increaseqty(elem){
-     elem.qty++;
-     result(data);
-     totalcal();
-}
+    div.append(image , cartshoedetails);
 
-function decreaseqty(elem){
-    elem.qty--;
-    if(elem.qty<=0){
-        elem.qty++;
-        alert("invalid qty");
-        return;
-    }
-    result(data);
-    totalcal();
-}
+
+    document.querySelector("#cartcontainer").append(div);
+
+
+});
+};
+
+
+// function increaseqty(elem){
+//      elem.qty++;
+//      result(data);
+//      totalcal();
+// }
+
+// function decreaseqty(elem){
+//     elem.qty--;
+//     if(elem.qty<=0){
+//         elem.qty++;
+//         alert("invalid qty");
+//         return;
+//     }
+//     result(data);
+//     totalcal();
+// }
+
+
+
 
 function totalcal(){
     document.querySelector("#total").innerText = "";
@@ -70,6 +120,10 @@ var total = document.createElement("h2");
 var res = data.reduce(function(acc, elem){
     return acc + (elem.qty * elem.price);
 },0);
-total.innerText = "Total price - ₹ " + res;
+total.innerText = "Total " +   " ₹" + res;
 document.querySelector("#total").append(total);
+}
+function quantitychange(quantity , elem){
+    elem.qty = quantity.value;
+    totalcal();
 }
